43b5be3680acd9ec8c4b80397881f671874b23b1
phadtaregauri24 <32060475+phadtaregauri24@users.noreply.github.com>
2017-09-18 20:53:00 +0530

    Add files via upload

---
+++ Login.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace teamChatProject.Account {
+    
+    
+    public partial class Login {
+        
+        /// <summary>
+        /// imgIcon control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgIcon;
+    }
+}
